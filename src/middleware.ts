60f44ee5db91a7cb4b0ca17fbd260e285f776ab8
laper01 <yusufyahya0107@gmail.com>
2024-04-17 15:24:28 +0800

    add to ignored route post get all

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ['/', '/favicon.ico'],
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/trpc/post.hello"]
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/trpc/post.hello", '/api/trpc/posts.getAll']
 });
 
 export const config = {
